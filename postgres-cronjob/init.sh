56da4781a41fa15452cf0f63621b3f6702b1a1e9
mishodzuliashvili <mishodzuliashvili1@gmail.com>
2025-05-16 18:01:46 +0400

    Add PostgreSQL backup functionality to cronjob script and update Docker Compose configuration

--- postgres-cronjob/init.sh
+++ postgres-cronjob/init.sh
@@ -1,2 +1,33 @@
 #!/bin/sh
-echo "📣 Cronjob ran at $(date) - printing index" >> /cron-output.txt
+
+PGUSER=${POSTGRES_USER}
+PGPASSWORD=${POSTGRES_PASSWORD}
+PGHOST=postgres-for-next-app
+PGDATABASE=${POSTGRES_DB}
+
+export PGPASSWORD
+
+pg_dump -h $PGHOST -U $PGUSER -d $PGDATABASE > /backups/backup_$(date +%F_%T).sql
+
+if [ $? -ne 0 ]; then
+    echo "❌ Backup failed at $(date)" >> /var/log/cron-output.txt
+    exit 1
+fi 
+
+# Compress the backup file
+gzip /backups/backup_$(date +%F_%T).sql
+if [ $? -ne 0 ]; then
+    echo "❌ Compression failed at $(date)" >> /var/log/cron-output.txt
+    exit 1
+fi
+# Remove old backups older than 7 days
+find /backups -type f -name "*.sql.gz" -mtime +7 -exec rm {} \;
+
+# Log the successful backup
+echo "✅ Backup and compression completed successfully at $(date)" >> /var/log/cron-output.txt
+# Check if the backup file exists
+if [ -f /backups/backup_$(date +%F_%T).sql.gz ]; then
+    echo "✅ Backup file exists at $(date)" >> /var/log/cron-output.txt
+else
+    echo "❌ Backup file does not exist at $(date)" >> /var/log/cron-output.txt
+fi
